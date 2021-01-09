94b43beed36e75f757e051ee7f77ce3903f9c87b
Wellington Henrik <wellingtonhenrik13@gmail.com>
2021-01-09 20:33:04 -0300

    Criação do projeto cadastro-produtos

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App (props) {   
+
+    const modificarNome = event=>{
+        console.log(event.target.value)
+    }
+
+    const criaComboBox = () =>{
+        const opcoes=["Fulano","Cicrano"]
+        const comboBoxOpcoes=opcoes.map(opcao=><option>{opcao}</option>)
+
+        return(
+            <select>
+                {comboBoxOpcoes}
+            </select>
+        )
+    }
+    
+    const MeuComboBox = () => criaComboBox()
+        return (
+            <> 
+            <input className="text-centralizado" type="text" value={props.nome} onChange={modificarNome}/>
+            <h1>Hello {props.nome} sua idade é {props.idade}</h1>
+            <MeuComboBox/>
+            </>
+        
+        )
+    }
+
 
 export default App;
